b5a8963b83e56a77e46efa0b4a45fcaa46562978
klash <klaus.schliep@gmail.com>
2018-12-05 18:35:43 -0500

    document example data set

---
+++ R/thinr-package.R
@@ -0,0 +1,14 @@
+#' Species occurrence data
+#' 
+#' Some species occurrence data from eBird.
+#' 
+#' 
+#' @name mx_2015
+#' @docType data
+#' @keywords datasets
+#' @examples
+#' 
+#' data(mx_2015)
+#' head(mx_2015)
+#' 
+NULL
